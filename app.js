c43513e78968eb5a0517839cf782a29cac74bf30
Elvira-del <ninelostr@gmail.com>
2022-11-01 00:28:51 +0300

    refactor: add class for list item

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var completedTasksHolder = document.querySelector(".completed-tasks"); //complet
 //New task list item
 var createNewTaskElement = function (taskString) {
   var listItem = document.createElement("li");
+  listItem.classList.add("todo__item");
 
   //input (checkbox)
   var checkBox = document.createElement("input"); //checkbx
